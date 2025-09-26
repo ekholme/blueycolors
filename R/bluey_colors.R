9ca316504ea1c56c20b9a5133de547d3841e70c5
Eric Ekholm <eric.ekholm@gmail.com>
2025-09-26 14:20:07 -0400

    fix issue with continuous color scale generation

--- R/bluey_colors.R
+++ R/bluey_colors.R
@@ -1,5 +1,5 @@
 #' Available Bluey Color Palettes
-#' 
+#'
 #' Use \code{\link{bluey_palette}} to extract palettes of the desired length
 #' @export
 bluey_colors <- list(
@@ -10,14 +10,14 @@ bluey_colors <- list(
 )
 
 #' Bluey Color Palette Constructor
-#' 
+#'
 #' @description Extract a color palette of the desired length
-#' 
+#'
 #' @param option The name of the color palette to use. Current options are 'bluey', 'chilli', 'heeler', and 'socks'
 #' @param n The number of colors to return. Currently, all scales are limited to 5 colors
-#' 
+#'
 #' @name bluey_palette
-#' 
+#'
 #' @export
 bluey_palette <- function(option = "bluey", n = NULL) {
   stopifnot(is.null(option) || option %in% names(bluey_colors))
@@ -26,14 +26,14 @@ bluey_palette <- function(option = "bluey", n = NULL) {
 
   if (is.null(n)) {
     return(pal)
-  }   
+  }
 
   if (n > length(pal)) {
     stop(paste0("Not enough colors in palette ", option))
   }
 
-  return(pal[1:n]) 
-} 
+  return(pal[1:n])
+}
 
 # helper to generate a palette for discrete scales
 palette_gen <- function(direction = 1, option = "bluey") {
@@ -43,15 +43,15 @@ palette_gen <- function(direction = 1, option = "bluey") {
     pal <- bluey_palette(option, n)
 
     if (direction >= 0) pal else rev(pal)
-
   }
 }
 
-#helper to generate palette for continuous scales
+# helper to generate palette for continuous scales
+# note that ... isn't currently used
 palette_gen_c <- function(direction = 1, option = "bluey", ...) {
   stopifnot(is.numeric(direction))
 
-  pal <- bluey_palette(option)[c(1, 2)]
+  pal <- bluey_palette(option)
 
   pal <- if (direction >= 0) pal else rev(pal)
 
@@ -59,36 +59,35 @@ palette_gen_c <- function(direction = 1, option = "bluey", ...) {
 }
 
 #' Bluey ggplot 2 color scales
-#' 
+#'
 #' @description
 #' ggplot2 fill and color scales derived from Bluey characters
-#' 
+#'
 #' - `scale_color_bluey()` and `scale_fill_bluey()` provide discrete fill functions
 #' - `scale_color_bluey_c()` and `scale_fill_bluey_c()` provide continuous fill functions`
-#' 
+#'
 #' @param option The name of the color palette to use. See \code{\link{bluey_palette}} for available options
 #' @param direction If greater than or equal to 0, it will return the standard direction; otherwise it will reverse the direction of the palette
 #' @param ... Additional arguments passed to ggplot2 scale functions
-#' 
+#'
 #' @name bluey_ggplot2_scales
-#' 
+#'
 #' @export
-#' 
+#'
 #' @examples
 #' library(ggplot2)
 #' tmp <- data.frame(x = rnorm(100), y = rnorm(100), z = rep(c("a", "b", "c", "d"), 25))
-#' 
+#'
 #' # using a discrete scale
 #' ggplot(tmp, aes(x, y, color = z)) +
-#' geom_point() +
-#' scale_color_bluey()
-#' 
+#'   geom_point() +
+#'   scale_color_bluey()
+#'
 #' # changing the palette used when creating a discrete scale
 #' ggplot(tmp, aes(x, y, color = z)) +
-#' geom_point() +
-#' scale_color_bluey(option = "socks")
-#' 
-#' 
+#'   geom_point() +
+#'   scale_color_bluey(option = "socks")
+#'
 scale_color_bluey <- function(option = "bluey", direction = 1, ...) {
   ggplot2::discrete_scale(
     aesthetics = "color",
@@ -99,7 +98,7 @@ scale_color_bluey <- function(option = "bluey", direction = 1, ...) {
 }
 
 #' @rdname bluey_ggplot2_scales
-#' @export 
+#' @export
 scale_fill_bluey <- function(option = "bluey", direction = 1, ...) {
   ggplot2::discrete_scale(
     aesthetics = "fill",
@@ -114,7 +113,7 @@ scale_fill_bluey <- function(option = "bluey", direction = 1, ...) {
 scale_color_bluey_c <- function(option = "bluey", direction = 1, ...) {
   pal <- palette_gen_c(direction = direction, option = option)
 
-  ggplot2::scale_color_gradient(colors = pal(256), ...)
+  ggplot2::scale_color_gradientn(colors = pal(256), ...)
 }
 
 #' @rdname bluey_ggplot2_scales
@@ -122,6 +121,5 @@ scale_color_bluey_c <- function(option = "bluey", direction = 1, ...) {
 scale_fill_bluey_c <- function(option = "bluey", direction = 1, ...) {
   pal <- palette_gen_c(direction = direction, option = option)
 
-  ggplot2::scale_fill_gradient(colors = pal(256), ...)
-
+  ggplot2::scale_fill_gradientn(colors = pal(256), ...)
 }
